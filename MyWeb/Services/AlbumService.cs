2022d5a35a7cbbf94f1e9673c65ac5b7546968f9
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-19 01:33:17 +0430

    Service

---
+++ MyWeb/Services/AlbumService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChinookLab.Protos;
+using Grpc.Core;
+using MyWeb.Models;
+
+namespace ServerApp.Services
+{
+    public class AlbumService : ChinookLab.Protos.AlbumsService.AlbumsServiceBase
+    {
+        private readonly ChinbookContext _db;
+        public AlbumService(ChinbookContext db)
+        {
+            _db = db;
+        }
+
+        public override async Task<AlbumReply> GetAlbum(AlbumRequest request, ServerCallContext context)
+        {
+            var model = await _db.Albums.FindAsync(request.AlbumId);
+            var resposne = new AlbumReply { AlbumId = model.AlbumId,ArtistId=model.ArtistId,Title=model.Title };
+
+            return resposne;
+        }
+    }
+}
--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -1,38 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MyWeb.Models;
 
-namespace MyWeb
+namespace TestApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-
-            services.AddControllersWithViews();
-            services.AddRazorPages();
             services.AddGrpc();
-
-            services.AddDbContext<ChinbookContext>(
-                options =>
-                options.UseSqlite("Data Source=chinook.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,23 +26,17 @@ namespace MyWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                endpoints.MapGrpcService<GreeterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
             });
         }
     }
