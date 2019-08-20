0826326437307335cc36a50ac094274770ba8585
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-21 02:09:22 +0430

    test

---
+++ TestApp/Services/AlbumService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChinookLab.Protos;
+using Grpc.Core;
+
+
+namespace ServerApp.Services
+{
+    public class AlbumService : ChinookLab.Protos.AlbumsService.AlbumsServiceBase
+    {
+        public override async Task<AlbumReply> GetAlbum(AlbumRequest request, ServerCallContext context)
+        {
+            await Task.Delay(1);
+            var resposne = new AlbumReply { AlbumId =1,ArtistId=10,Title="Hello from server" };
+
+            return resposne;
+        }
+    }
+}
--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ServerApp.Services;
 
 namespace TestApp
 {
@@ -31,7 +32,7 @@ namespace TestApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<AlbumService>();
 
                 endpoints.MapGet("/", async context =>
                 {
