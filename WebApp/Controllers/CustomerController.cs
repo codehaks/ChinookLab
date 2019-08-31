d960dc710e4ea67f0613c59e23074681fcf3ac2f
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-31 06:08:17 +0430

    app

---
+++ WebApp/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CustomerController : ControllerBase
+    {
+        [Route("api/customer")]
+        public IActionResult Index()
+        {
+            return Ok("Done!");
+        }
+    }
+}
